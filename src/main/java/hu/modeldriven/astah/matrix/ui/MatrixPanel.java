aa40f11f81d409c6c880bc9441314c65e5078a96
Zsolt Sandor <zsolt.sandor@gmail.com>
2023-12-24 07:24:05 +0100

    Initial commit

---
+++ src/main/java/hu/modeldriven/astah/matrix/ui/MatrixPanel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hu.modeldriven.astah.matrix.ui;
+
+/**
+ *
+ * @author zsolt
+ */
+public class MatrixPanel extends AbstractMatrixPanel {
+    
+}
