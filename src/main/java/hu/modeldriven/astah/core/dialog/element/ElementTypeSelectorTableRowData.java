d556cb0e74ae730848a702986f0533af9c3a9b7c
Zsolt Sandor <zsolt.sandor@gmail.com>
2023-12-26 15:43:36 +0100

    Added block

--- src/main/java/hu/modeldriven/astah/core/dialog/element/ElementTypeSelectorTableRowData.java
+++ src/main/java/hu/modeldriven/astah/core/dialog/element/ElementTypeSelectorTableRowData.java
@@ -1,9 +1,12 @@
 package hu.modeldriven.astah.core.dialog.element;
 
 import com.change_vision.jude.api.inf.model.IAction;
+import com.change_vision.jude.api.inf.model.IClass;
 import com.change_vision.jude.api.inf.model.IRequirement;
 import com.change_vision.jude.api.inf.model.IUseCase;
 import hu.modeldriven.astah.core.dialog.element.matcher.ClassMatcher;
+import hu.modeldriven.astah.core.dialog.element.matcher.CombinedMatcher;
+import hu.modeldriven.astah.core.dialog.element.matcher.StereotypeMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,10 @@ public class ElementTypeSelectorTableRowData implements Supplier<List<ElementTyp
         rows.add(new ElementTypeSelectorTableRow("Activity", new ClassMatcher(IAction.class)));
         rows.add(new ElementTypeSelectorTableRow("Requirement", new ClassMatcher(IRequirement.class)));
         rows.add(new ElementTypeSelectorTableRow("Use Case", new ClassMatcher(IUseCase.class)));
+        rows.add(new ElementTypeSelectorTableRow("Block", new CombinedMatcher(
+                new ClassMatcher(IClass.class),
+                new StereotypeMatcher("block"))
+        ));
 
         return rows;
     }
